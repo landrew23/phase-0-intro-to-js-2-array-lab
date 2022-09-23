aad989542f851a5c4e158ea00e893ec700f356fe
William Killett <williamkillettiii@yahoo.com>
2022-09-23 08:00:40 -0700

    commit tree 747dbdb24edbf72c09714546089f124a858b67ec

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+cats.length=0;
+cats.push("Milo", "Otis", "Garfield");
+
+
+function destructivelyAppendCat(go){
+go = cats.push('Ralph')
+return go
+}
+function destructivelyPrependCat(stop){
+ stop = cats.unshift('Bob')  
+ return stop 
+}
+function destructivelyRemoveLastCat(bye){
+bye = cats.pop()
+return bye
+}
+function destructivelyRemoveFirstCat(well){
+    well = cats.shift()
+    return well
+}
+function appendCat(yes){
+ yes = cats.slice();
+ let result = [...cats,"Broom" ];
+ return result
+
+}
+function prependCat(no){
+no = cats.slice();
+let result = ["Arnold", ...cats]
+return result
+}
+
+function removeLastCat(front){
+ front = cats.slice(0,2);
+ return front   
+}
+
+function removeFirstCat(back){
+ back = cats.slice(1);
+ return back   
+}
